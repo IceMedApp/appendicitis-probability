0ca5fa4f68bda9be10cf843810eb4208b5ade6b7
RaggiBjo90 <ragnarbjoernsson90@gmail.com>
2025-08-06 13:30:02 -0400

    trying out unit testing

---
+++ tests/unit/appendicitis.test.js
@@ -0,0 +1,10 @@
+import { expect, test } from 'vitest'
+import { app_probability } from '../../functions/appendicitis'
+
+test('risk below 1', () => {
+  expect(app_probability(0.4)).toBe(0.19)
+})
+
+test('risk over 1', () => {
+  expect(app_probability(2.5)).toBe(0.79)
+})
