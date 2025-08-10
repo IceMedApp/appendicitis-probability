70c7fd71e4e2de3968825f4181ad0e28b57225e5
RaggiBjo90 <ragnarbjoernsson90@gmail.com>
2025-08-10 09:51:24 -0400

    Update appendicitis.test.js

--- tests/unit/appendicitis.test.js
+++ tests/unit/appendicitis.test.js
@@ -10,11 +10,11 @@ class Risk_factor_item {
   }
 }
 
-test('risk below 1', () => {
+test('risk < 1', () => {
   expect(app_probability(0.4)).toBe(0.19)
 })
 
-test('risk over 1', () => {
+test('risk > 1', () => {
   expect(app_probability(2.5)).toBe(0.79)
 })
 
@@ -22,7 +22,7 @@ test('risk = 1', () => {
   expect(app_probability(1)).toBe(0.49)
 })
 
-test('crp below 10', () => {
+test('crp < 10', () => {
   const crp = new Risk_factor_item(6.4);
   expect(crp_factor_app(crp)).toBe(0.741195658058578)
 })
